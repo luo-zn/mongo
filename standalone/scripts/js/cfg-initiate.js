6ceb1a5904fa9eb3d9e50724d8d7af292f45cf55
Zhennan <zhennan.luo@HQ.TA-MP.COM>
2020-03-16 20:20:50 +0800

    fix standalone/scripts/js/cfg-initiate.js

--- standalone/scripts/js/cfg-initiate.js
+++ standalone/scripts/js/cfg-initiate.js
@@ -3,7 +3,7 @@ rs.initiate({
     configsvr: true, 
     members: [
         { _id : 0, host : "cfg1:27019" },
-        { _id : 1, host : "cfg1:27019" },
-        { _id : 2, host : "cfg1:27019" }
+        { _id : 1, host : "cfg2:27019" },
+        { _id : 2, host : "cfg3:27019" }
     ]
 });
